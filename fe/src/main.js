b6634688ed93eb61cc99abf3c763bbe6872a3b23
alan_wang <sz9751210@gmail.com>
2024-09-02 05:04:58 +0000

    bug - 修正 resize observer loop error

--- fe/src/main.js
+++ fe/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as ELIcons from "@element-plus/icons-vue";
@@ -12,6 +11,19 @@ import { GlobalCmComponent } from "codemirror-editor-vue3";
 import "codemirror/theme/idea.css";
 // 引入語言模式 可以從 codemirror/mode/ 下引入多個
 import "codemirror/mode/yaml/yaml.js";
+
+const observerErrorHandler = (error) => {
+  if (error.message.includes("ResizeObserver loop limit exceeded")) {
+    // 忽略此類錯誤
+    return;
+  }
+  console.error(error);
+};
+
+// 添加全局錯誤處理
+window.addEventListener("error", observerErrorHandler);
+window.addEventListener("unhandledrejection", observerErrorHandler);
+
 
 const app = createApp(App);
 for (const iconName in ELIcons) {
